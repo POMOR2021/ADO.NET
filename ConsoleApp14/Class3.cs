2f505adf332b8f9e39ebdd9f0aef1be38f6664d8
POMOR2021 <93044445+POMOR2021@users.noreply.github.com>
2025-02-03 23:20:26 +0300

    Delete ConsoleApp14 directory

--- ConsoleApp14/Class3.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConsoleApp14  
-{
-    public static class StringConnection
-    {
-        public static string ConnectionString => "Data Source=ELFOREVER\\SQLEXPRESS;DataBase=DBBOOKS;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
